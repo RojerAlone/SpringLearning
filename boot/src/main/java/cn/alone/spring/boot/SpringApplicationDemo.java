1424a3f9c975c940b54bb92dbfd9d7e4935a2a62
rojeralone <rojeralone@163.com>
2020-02-15 16:20:15 +0800

    add spring-boot

---
+++ boot/src/main/java/cn/alone/spring/boot/SpringApplicationDemo.java
@@ -0,0 +1,13 @@
+package cn.alone.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplicationDemo.class, args);
+    }
+
+}
